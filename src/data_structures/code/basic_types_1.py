e3e712b960343d418ee7a45357f00bbcbadeb767
fapfood <fapfood@gmail.com>
2018-09-11 23:47:50 +0200

    add data structures

---
+++ src/data_structures/code/basic_types_1.py
@@ -0,0 +1,10 @@
+var1 = 3.14
+var2 = 123
+var3 = 'abcd'
+var4 = True
+var5 = -1 - 3.44j
+print(type(var1))  # <class 'float'>
+print(type(var2))  # <class 'int'>
+print(type(var3))  # <class 'str'>
+print(type(var4))  # <class 'bool'>
+print(type(var5))  # <class 'complex'>
---
+++ src/data_structures/code/basic_types_2.py
@@ -0,0 +1,13 @@
+var1 = True
+res1 = 2 + 4
+res2 = res1 + 3.14
+res3 = 'abcd' * 2
+res4 = not var1
+print(type(res1))  # <class 'float'>
+print(type(res2))  # <class 'int'>
+print(type(res3))  # <class 'str'>
+print(type(res4))  # <class 'bool'>
+print(res1)  # 6
+print(res2)  # 9.14
+print(res3)  # abcdabcd
+print(res4)  # False
